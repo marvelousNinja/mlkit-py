a1d5c67168704574b25359615a66ec8277ba5612
Alexander Sologub <alexsologoub@gmail.com>
2017-12-13 23:02:48 +0300

    Extract distance to var

--- mlkit/knn_regressor.py
+++ mlkit/knn_regressor.py
@@ -4,13 +4,13 @@ class KnnRegressor():
   def fit(self, X, y):
     self.X = np.array(X)
     self.y = np.array(y)
+    self.distance = lambda a, b: np.sqrt(np.sum((a - b) ** 2))
 
   def predict(self, X):
-    distance = lambda a, b: np.sqrt(np.sum((a - b) ** 2))
     predictions = []
 
     for x in X:
-      distances = [distance(x, _x) for _x in self.X]
+      distances = [self.distance(x, _x) for _x in self.X]
       order = np.argsort(distances)
       nearest = self.y[order][-5:]
       predictions.append(np.mean(nearest))
