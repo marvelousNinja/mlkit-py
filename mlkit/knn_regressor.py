88a286a50a94b5e79ef8c4b86a3314ddfa54b902
Alexander Sologub <alexsologoub@gmail.com>
2017-12-13 16:37:09 +0300

    Add naive KNN regressor

---
+++ mlkit/knn_regressor.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+class KnnRegressor():
+  def fit(self, X, y):
+    self.X = np.array(X)
+    self.y = np.array(y)
+
+  def predict(self, X):
+    distance = lambda a, b: np.sqrt(np.sum((a - b) ** 2))
+    predictions = []
+
+    for x in X:
+      distances = [distance(x, _x) for _x in self.X]
+      order = np.argsort(distances)
+      nearest = self.y[order][-5:]
+      predictions.append(np.mean(nearest))
+    return predictions
+
+
+def test_init():
+  KnnRegressor()
+
+def test_fit():
+  model = KnnRegressor()
+  model.fit([[0, 0], [1, 1]], [1, 2])
+
+def test_predict():
+  model = KnnRegressor()
+  model.fit([0], [1])
+  assert model.predict([0]) == [1.0]
