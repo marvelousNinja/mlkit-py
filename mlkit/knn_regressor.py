7dbd73eade43314b7953044c2aae81b251487ede
Alexander Sologub <alexsologoub@gmail.com>
2017-12-14 17:03:13 +0300

    Add basic regression metrics

--- mlkit/knn_regressor.py
+++ mlkit/knn_regressor.py
@@ -5,6 +5,7 @@ class KnnRegressor():
     self.X = np.array(X)
     self.y = np.array(y)
     self.distance = lambda a, b: np.sqrt(np.sum((a - b) ** 2))
+    self.n = 5
 
   def predict(self, X):
     predictions = []
@@ -12,19 +13,6 @@ class KnnRegressor():
     for x in X:
       distances = [self.distance(x, _x) for _x in self.X]
       order = np.argsort(distances)
-      nearest = self.y[order][-5:]
+      nearest = self.y[order][:self.n]
       predictions.append(np.mean(nearest))
     return predictions
-
-
-def test_init():
-  KnnRegressor()
-
-def test_fit():
-  model = KnnRegressor()
-  model.fit([[0, 0], [1, 1]], [1, 2])
-
-def test_predict():
-  model = KnnRegressor()
-  model.fit([0], [1])
-  assert model.predict([0]) == [1.0]
---
+++ mlkit/metrics.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def mse(y_true, y_pred):
+  return np.mean((y_true - y_pred) ** 2)
+
+def rmse(y_true, y_pred):
+  return np.sqrt(mse(y_true, y_pred))
+
+def mae(y_true, y_pred):
+  return np.mean(np.absolute(y_true - y_pred))
