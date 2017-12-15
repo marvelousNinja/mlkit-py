e3ae2445caf1e624d7cb44153707d0cd38f8e647
Alexander Sologub <alexsologoub@gmail.com>
2017-12-15 23:02:21 +0300

    Add closed-form solution to linear regression

---
+++ mlkit/normal_equation_regressor.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class NormalEquationRegressor():
+  def fit(self, X, y):
+    X = np.c_[X, np.ones(len(X))]
+    self.w = np.linalg.inv(X.T @ X) @ X.T @ y
+
+  def predict(self, X):
+    X = np.c_[X, np.ones(len(X))]
+    return X @ self.w
---
+++ mlkit/normal_equation_regressor_test.py
@@ -0,0 +1,24 @@
+import pytest
+import os
+import pandas as pd
+import numpy as np
+
+from .normal_equation_regressor import NormalEquationRegressor
+from .metrics import mae
+from .util import make_scaler
+
+def test_on_iris():
+  full_path = os.path.realpath(__file__)
+  dir_name = os.path.dirname(full_path)
+  iris = pd.read_csv(dir_name + '/datasets/boston_house_prices.csv')
+
+  X = iris.drop('MEDV', axis=1).values
+  y = iris['MEDV'].values
+
+  scale = make_scaler(X[:400])
+
+  model = NormalEquationRegressor()
+
+  model.fit(scale(X[:400]), y[:400])
+  y_pred = model.predict(scale(X[400:]))
+  assert mae(y[400:], y_pred) == pytest.approx(5, 0.1)
