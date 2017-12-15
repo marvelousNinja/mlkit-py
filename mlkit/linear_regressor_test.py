5193aba31e573fe1dc88e28a74e5cb265537b6c1
Alexander Sologub <alexsologoub@gmail.com>
2017-12-15 20:12:48 +0300

    Add naive implementation of linear regression

---
+++ mlkit/linear_regressor_test.py
@@ -0,0 +1,20 @@
+import pytest
+import os
+import pandas as pd
+import numpy as np
+
+from .linear_regressor import LinearRegressor
+from .metrics import mae
+
+def test_on_iris():
+  full_path = os.path.realpath(__file__)
+  dir_name = os.path.dirname(full_path)
+  iris = pd.read_csv(dir_name + '/datasets/boston_house_prices.csv')
+
+  X = iris.drop('MEDV', axis=1).values
+  y = iris['MEDV'].values
+
+  model = LinearRegressor()
+  model.fit(X[:400], y[:400])
+  y_pred = model.predict(X[400:])
+  assert mae(y[400:], y_pred) == pytest.approx(6.74, 0.1)
