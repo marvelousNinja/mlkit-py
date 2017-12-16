de7bf47b2d3780710c64433fa2b591c2da32691b
Alexander Sologub <alexsologoub@gmail.com>
2017-12-16 20:18:33 +0300

    Add tests for decision tree regression

--- mlkit/decision_tree_regressor.py
+++ mlkit/decision_tree_regressor.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 class DecisionTreeRegressor():
-  def fit(X, y):
+  def fit(self, X, y):
     self.root = make_node(X, y)
 
-  def predict(X):
+  def predict(self, X):
     return self.root(X)
 
 def make_node(X, y):
@@ -24,8 +24,8 @@ def make_node(X, y):
   def decision(X):
     predictions = np.zeros(len(X))
     mask = split(X)
-    predictions[mask] = right(X)
-    predictions[~mask] = left(X)
+    predictions[mask] = right(X[mask])
+    predictions[~mask] = left(X[~mask])
     return predictions
 
   return decision
@@ -33,21 +33,38 @@ def make_node(X, y):
 def find_best_split(X, y):
   # TODO AS: Criterion needs to be parametrized
   # TODO AS: Criterion - the less it is => the better?
+  min_leaf_samples = 5
   curr_value = criterion(y)
   curr_split = None
 
-  for column in len(X[0]):
+  for column in range(len(X[0])):
     # TODO AS: Split point generator needs to be parametrized
-    for value in np.unique(X[, column]):
+    for value in get_split_candidates(column, X, y):
       # TODO AS: Split condition generator needs to pe parametrized
-      split = lambda X: X[, column] >= value
+      split = make_split(column ,value)
       mask = split(X)
       y_right = y[mask]
       y_left = y[~mask]
 
+      if len(y_right) < min_leaf_samples or len(y_left) < min_leaf_samples:
+        continue
+
+      # TODO AS: Min leaf values should be encorporated here
       split_value = (criterion(y_right) * len(y_right) + criterion(y_left) * len(y_left)) / len(y)
-      if (split_value < curr_split):
+      # TODO AS: Min gain should be encorporated here
+      if (split_value < curr_value):
         curr_split = split
         curr_value = split_value
 
   return curr_split
+
+# TODO AS: Yeah, sure
+def get_split_candidates(column, X, y):
+  return [np.mean(X[:, column])]
+
+def make_split(column, value):
+  return lambda X: X[:, column] >= value
+
+def criterion(y):
+  mean = np.mean(y)
+  return np.mean((y - mean) ** 2)
