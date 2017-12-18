754931bcc6a1b7c90f8694f10fca9d554aa7aca5
Alexander Sologub <alexsologoub@gmail.com>
2017-12-18 23:18:04 +0300

    Add RMLSE

--- mlkit/metrics.py
+++ mlkit/metrics.py
@@ -8,3 +8,6 @@ def rmse(y_true, y_pred):
 
 def mae(y_true, y_pred):
   return np.mean(np.absolute(y_true - y_pred))
+
+def rmsle(y_true, y_pred):
+  return np.sqrt(np.mean((np.log(y_pred + 1) - np.log(y_true + 1)) ** 2))
