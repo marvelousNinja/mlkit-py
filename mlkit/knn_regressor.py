b89f9b0489a346b3f076fae4d93685e966d7ccef
Alexander Sologub <alexsologoub@gmail.com>
2018-01-09 17:35:20 +0300

    Vectorize the KNN regressor

--- mlkit/knn_regressor.py
+++ mlkit/knn_regressor.py
@@ -1,18 +1,20 @@
 import numpy as np
 
+def euclidean(A, B):
+    return np.sqrt(-2 * (A @ B.T) + np.sum(A ** 2, axis=1).reshape(-1, 1) + np.sum(B ** 2, axis=1))
+
 class KnnRegressor():
+    def __init__(self, k_neighbours=5, distance=euclidean):
+        self.X_train = None
+        self.y_train = None
+        self.k_neighbours = k_neighbours
+        self.distance = distance
+
     def fit(self, X, y):
-        self.X = np.array(X)
-        self.y = np.array(y)
-        self.distance = lambda a, b: np.sqrt(np.sum((a - b) ** 2))
-        self.n = 5
+        self.X_train = X
+        self.y_train = y
 
     def predict(self, X):
-        predictions = []
-
-        for x in X:
-            distances = [self.distance(x, _x) for _x in self.X]
-            order = np.argsort(distances)
-            nearest = self.y[order][:self.n]
-            predictions.append(np.mean(nearest))
-        return predictions
+        distances = self.distance(X, self.X_train)
+        neighbour_indicies = np.argsort(distances, axis=1)[:, :self.k_neighbours]
+        return [np.mean(self.y_train[neighbour_indicies[i]]) for i in range(len(X))]
